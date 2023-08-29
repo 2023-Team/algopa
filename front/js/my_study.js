7a87f5ab9448eac2d8ee86bff6b57bed8e7f6e52
yoon-0409 <quddbs0409@gmail.com>
2023-08-30 03:23:22 +0900

    힘드러요.. my_study 틀 다 잡았음 -뱅-

---
+++ front/js/my_study.js
@@ -0,0 +1,21 @@
+const sidebar = document.querySelector('#side-bar')
+const spans = sidebar.querySelectorAll('span')
+
+console.log(spans)
+function 확장() {
+  console.log('확장')
+  spans.forEach((span) => {
+    span.classList.toggle('hidden')
+    span.style.color = 'pink'
+  })
+}
+function 축소() {
+  console.log('축소')
+  spans.forEach((span) => {
+    span.classList.add('hidden')
+    span.style.color = 'black'
+  })
+}
+
+sidebar.addEventListener('mouseleave', 축소)
+sidebar.addEventListener('mouseenter', 확장)
