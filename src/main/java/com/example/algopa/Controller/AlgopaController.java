c30744d78556b15636691c97fcf1817c5b1b2137
sunha park <sunhapark1004@gmail.com>
2023-09-03 18:28:44 +0900

    index 페이지에 banner 클래스 완성 - 박선하

--- src/main/java/com/example/algopa/Controller/AlgopaController.java
+++ src/main/java/com/example/algopa/Controller/AlgopaController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AlgopaController {
-    @GetMapping("my_study_copy")
+    @GetMapping("/my_study_copy")
     public String my_study_copy(){
         return "my_study_copy";
     }
-    @GetMapping("my_study")
+    @GetMapping("/my_study")
     public String my_study(){
         return "my_study";
     }
-    @GetMapping("login")
+    @GetMapping("/login")
     public String login(){
         return "login";
     }
-
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
