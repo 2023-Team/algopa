68216abfc9589a8ca4a8d015e9f4d9673b7f3a2c
yoon-0409 <quddbs0409@gmail.com>
2023-09-08 20:27:10 +0900

    수정본

--- src/main/java/com/example/algopa/Controller/AlgopaController.java
+++ src/main/java/com/example/algopa/Controller/AlgopaController.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AlgopaController {
-<<<<<<< HEAD
     @GetMapping("my_study_submit")
-    public String my_study_copy(){
-        return "my_study_submit";
-=======
-    @GetMapping("/my_study_copy")
-    public String my_study_copy(){
-        return "my_study";
->>>>>>> c87b0075d113a706733096923b65fb2e26667246
-    }
+    public String my_study_submit(){
+        return "my_study_submit";}
+
     @GetMapping("/my_study")
     public String my_study(){
         return "my_study";
